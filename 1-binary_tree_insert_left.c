632ebc921da1a1f32e9086411abe02bd2aa1aee9
nyacho04 <dnachod83@gmail.com>
2024-08-06 14:19:07 +0000

    commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,8 +28,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		parent->left->parent = new_node;
 		new_node->left = parent->left;
-		new_node->parent = parent;
-		parent->left = new_node;
+		/*new_node->parent = parent;
+		parent->left = new_node;*/
 
 	}
 

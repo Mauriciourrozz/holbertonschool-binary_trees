dafd7d8fb035065aacbb3ad1e7dd8054cec31b65
Mauriciourrozz <9366@holbertonstudents.com>
2024-08-06 15:15:45 +0000

    commit

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserta un nodo como  hijo izquierdo de otro nodo
+ * @parent: puntero al nodo para insertar el nodo
+ * @value: Valor a almacenar el nuevo nodo
+ * Return: Valor del nuevonodo
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
+	if (parent->right != NULL)
+	{
+		parent->right->parent = new_node;
+		new_node->right = parent->right;
+	}
+	parent->right = new_node;
+
+	return (new_node);
+}

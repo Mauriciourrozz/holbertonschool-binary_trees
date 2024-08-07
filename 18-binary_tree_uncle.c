bc47b8ea711d4f8755bfa4bb5c0410d9ccaf7785
Mauriciourrozz <9366@holbertonstudents.com>
2024-08-07 17:53:35 +0000

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,12 +6,14 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent = node->parent->parent;
+	binary_tree_t *grandparent;
 
-	if (node == NULL || node->parent == NULL || grandparent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
+
+	grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent)
 	{

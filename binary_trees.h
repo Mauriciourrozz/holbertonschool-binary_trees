aa87f2a54bb182483269ae66d571a3659649ec8e
nyacho04 <dnachod83@gmail.com>
2024-08-07 13:51:36 +0000

    commit

--- binary_trees.h
+++ binary_trees.h
@@ -37,4 +37,5 @@ size_t traverse_node(const binary_tree_t *tree, size_t count, size_t count2);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
 #endif

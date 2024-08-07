d88e6b72099e4a2c026b92caba0b347109918efb
Mauriciourrozz <9366@holbertonstudents.com>
2024-08-07 17:45:34 +0000

    18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - it finds the grandparent of a node
+ * @node: The node to find the grandparent
+ * Return: Is null if the node is null or if the node has no grandparent
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grandparent = node->parent->parent;
+
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (grandparent->left == node->parent)
+	{
+		return (grandparent->right);
+	}
+	else
+	{
+		return (grandparent->left);
+	}
+
+}
